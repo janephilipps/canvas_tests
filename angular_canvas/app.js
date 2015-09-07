56a3f94655d424b9240680dfa6389eb18877e21a
Jane Philipps <jane.philipps@gmail.com>
2015-09-07 00:00:05 -0700

    trying to make canvas work with angular

--- angular_canvas/app.js
+++ angular_canvas/app.js
@@ -1,5 +1,31 @@
 var app = angular.module("angularCanvas", []);
 
 app.controller("CanvasCtrl", ["$scope", function ($scope) {
-  $scope.test = "I'm a canvas!";
+
+  $scope.painting = [["blue", "cyan", "yellow"],
+                     ["blue", "cyan", "yellow"],
+                     ["blue", "cyan", "yellow"]];
+
+  $scope.render = function () {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    var data = document.getElementById('svg').innerHTML;
+    console.log(data);
+
+    var DOMURL = window.URL || window.webkitURL || window;
+
+    var img = new Image();
+    var svg = new Blob([data], {type: 'image/svg+xml;charset=utf-8'});
+    var url = DOMURL.createObjectURL(svg);
+
+    img.onload = function () {
+      ctx.drawImage(img, 0, 0);
+      DOMURL.revokeObjectURL(url);
+    }
+
+    img.src = url;
+  };
+
+
 }]);
--- drawing_DOM_objects_into_canvas/script.js
+++ drawing_DOM_objects_into_canvas/script.js
@@ -1,70 +1,76 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-var data = '<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">' +
-           '<foreignObject width="100%" height="100%" style="background-color:white;">' +
-             '<div xmlns="http://www.w3.org/1999/xhtml" style="display:block;">' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:yellow;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:yellow;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-             '</div>' +
-             '<div xmlns="http://www.w3.org/1999/xhtml" style="display:block;">' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-             '</div>' +
-             '<div xmlns="http://www.w3.org/1999/xhtml" style="display:block;">' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:yellow;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:yellow;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-             '</div>' +
-             '<div xmlns="http://www.w3.org/1999/xhtml" style="display:block;">' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:yellow;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:yellow;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-             '</div>' +
-             '<div xmlns="http://www.w3.org/1999/xhtml" style="display:block;">' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:yellow;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-               '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
-               '</div>' +
-             '</div>' +
-           '</foreignObject>' +
-           '</svg>';
+
+
+// Get well-formed markup
+var data = document.getElementById('svg2').innerHTML;
+
+
+// var data = '<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">' +
+//            '<foreignObject width="100%" height="100%" style="background-color:white;">' +
+//              '<div xmlns="http://www.w3.org/1999/xhtml" style="display:block;">' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:yellow;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:yellow;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//              '</div>' +
+//              '<div xmlns="http://www.w3.org/1999/xhtml" style="display:block;">' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//              '</div>' +
+//              '<div xmlns="http://www.w3.org/1999/xhtml" style="display:block;">' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:yellow;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:yellow;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//              '</div>' +
+//              '<div xmlns="http://www.w3.org/1999/xhtml" style="display:block;">' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:yellow;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:yellow;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//              '</div>' +
+//              '<div xmlns="http://www.w3.org/1999/xhtml" style="display:block;">' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:yellow;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:blue;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//                '<div xmlns="http://www.w3.org/1999/xhtml" style="width:25px;height:25px;background-color:cyan;border-radius:50%;display:inline-block;margin:5px;">' +
+//                '</div>' +
+//              '</div>' +
+//            '</foreignObject>' +
+//            '</svg>';
 
 var DOMURL = window.URL || window.webkitURL || window;
 
